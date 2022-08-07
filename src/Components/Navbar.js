594189b369a9fceee4de606cdec74d07db81de39
animeshgupta971 <you@example.com>
2022-08-08 01:34:06 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+const Navbar = () => {
+  return (
+    <>
+    <nav className="">
+            <div className="">
+                <ul className="nav--wrapper">
+                    <li className="nav--item">
+                        <Link className="nav--link" to="/">Home</Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link className="nav--link" to="/about">About</Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link className="nav--link" to="/services">Services</Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link className="nav--link" to="/skills">Skills</Link>
+                    </li>
+                    <li className="nav--item">
+                        <Link className="nav--link" to="/Contact">Conatct US</Link>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+</>
+  )
+}
+
+export default Navbar
---
+++ src/SkillsPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import TechnicalSkills from './Components/TechnicalSkills'
+
+const SkillsPage = () => {
+  return (
+<>
+<div className="spacebar"></div>
+<Navbar></Navbar>
+<TechnicalSkills></TechnicalSkills>
+</>
+    )
+}
+
+export default SkillsPage
